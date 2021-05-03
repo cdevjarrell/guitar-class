a7197cb8580d278fbe5b8aefcf83784a03026bf9
cdevjarrell <christian.j.jarrell@gmail.com>
2021-05-03 17:39:41 -0400

    Create MainRouter

--- client/course/api-course.js
+++ client/course/api-course.js
@@ -1,87 +1,136 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import HomeIcon from "@material-ui/icons/Home";
-import Library from "@material-ui/icons/LocalLibrary";
-import Button from "@material-ui/core/Button";
-import auth from "./../auth/auth-helper";
-import { Link, withRouter } from "react-router-dom";
-
-const isActive = (history, path) => {
-  if (history.location.pathname == path) return { color: "#f57c00" };
-  else return { color: "#fffde7" };
+const create = async (params, credentials, course) => {
+  try {
+    let response = await fetch("/api/courses/by/" + params.userId, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: course,
+    });
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
 };
-const isPartActive = (history, path) => {
-  if (history.location.pathname.includes(path))
-    return { color: "#fffde7", backgroundColor: "#f57c00", marginRight: 10 };
-  else
-    return {
-      color: "#616161",
-      backgroundColor: "#fffde7",
-      border: "1px solid #f57c00",
-      marginRight: 10,
-    };
-};
-const Menu = withRouter(({ history }) => (
-  <AppBar position="fixed" style={{ zIndex: 12343455 }}>
-    <Toolbar>
-      <Typography variant="h6" color="inherit">
-        Christian's Guitar Academy
-      </Typography>
-      <div>
-        <Link to="/">
-          <IconButton aria-label="Home" style={isActive(history, "/")}>
-            <HomeIcon />
-          </IconButton>
-        </Link>
-      </div>
-      <div style={{ position: "absolute", right: "10px" }}>
-        <span style={{ float: "right" }}>
-          {!auth.isAuthenticated() && (
-            <span>
-              <Link to="/signup">
-                <Button style={isActive(history, "/signup")}>Sign up</Button>
-              </Link>
-              <Link to="/signin">
-                <Button style={isActive(history, "/signin")}>Sign In</Button>
-              </Link>
-            </span>
-          )}
-          {auth.isAuthenticated() && (
-            <span>
-              {auth.isAuthenticated().user.educator && (
-                <Link to="/teach/courses">
-                  <Button style={isPartActive(history, "/teach/")}>
-                    <Library /> Teach
-                  </Button>
-                </Link>
-              )}
-              <Link to={"/user/" + auth.isAuthenticated().user._id}>
-                <Button
-                  style={isActive(
-                    history,
-                    "/user/" + auth.isAuthenticated().user._id
-                  )}
-                >
-                  My Profile
-                </Button>
-              </Link>
-              <Button
-                color="inherit"
-                onClick={() => {
-                  auth.clearJWT(() => history.push("/"));
-                }}
-              >
-                Sign out
-              </Button>
-            </span>
-          )}
-        </span>
-      </div>
-    </Toolbar>
-  </AppBar>
-));
 
-export default Menu;
+const list = async (signal) => {
+  try {
+    let response = await fetch("/api/courses/", {
+      method: "GET",
+      signal: signal,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const read = async (params, signal) => {
+  try {
+    let response = await fetch("/api/courses/" + params.courseId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const update = async (params, credentials, course) => {
+  try {
+    let response = await fetch("/api/courses/" + params.courseId, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: course,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch("/api/courses/" + params.courseId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const listByInstructor = async (params, credentials, signal) => {
+  try {
+    let response = await fetch("/api/courses/by/" + params.userId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const newLesson = async (params, credentials, lesson) => {
+  try {
+    let response = await fetch(
+      "/api/courses/" + params.courseId + "/lesson/new",
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + credentials.t,
+        },
+        body: JSON.stringify({ lesson: lesson }),
+      }
+    );
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+const listPublished = async (signal) => {
+  try {
+    let response = await fetch("/api/courses/published", {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export {
+  create,
+  list,
+  read,
+  update,
+  remove,
+  listByInstructor,
+  newLesson,
+  listPublished,
+};
